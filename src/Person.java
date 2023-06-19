bca9b30efe2696be83cf1371744b81a2c78467ed
Vincent <vincent14@student.umn.ac.id>
2023-06-19 20:40:18 +0700

    field, method

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,9 @@
 class Person {
+  String name;
+  String address;
+  final String country = "Indonesia";
 
+  void sayHello(String paramName) {
+    System.out.println("Hello " + paramName + " , My name is " + name);
+  }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,13 +2,22 @@ public class PersonApp {
   public static void main(String[] args) {
 
     var person1 = new Person();
+    person1.name = "Vincent";
+    person1.address = "Pontianak";
+
     Person person2 = new Person();
+    person2.name = "bambang";
+    person2.address = "kakok";
+    person2.sayHello("atok");
 
     Person person3;
     person3 = new Person();
 
-    System.out.println(person1);
-    System.out.println(person2);
-    System.out.println(person3);
+    System.out.println(person1.name);
+    System.out.println(person1.address);
+    System.out.println(person1.country);
+    System.out.println(person2.address);
+    System.out.println(person3.address);
+    person3.sayHello("jojo");
   }
 }
