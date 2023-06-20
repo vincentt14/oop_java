297530a4ba1c2bf2aa030cb74c174a729aef3bb0
Vincent <vincent14@student.umn.ac.id>
2023-06-20 09:25:25 +0700

    var hiding, package

---
+++ src/ParentApp.java
@@ -0,0 +1,13 @@
+public class ParentApp {
+  public static void main(String[] args) {
+
+    Child child = new Child();
+    child.name = "Vincent";
+    child.doIt();
+    System.out.println(child.name);
+
+    Parent parent = (Parent) child;
+    parent.doIt();
+    System.out.println(parent.name);
+  }
+}
